55a3ada981467cc6fc3962b63d72ab9081ae93ad
alierenats <aliernates@gmail.com>
2023-07-28 22:00:11 +0300

    Initial Commit

---
+++ Collection-TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection-TableView
+//
+//  Created by Eren Ates on 28.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
